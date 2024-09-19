e11bb0df30ee974b939fbe54a7b048a3569e03a9
Thandolwethu <thandwem045@gmail.com>
2024-09-19 09:25:42 +0200

    Updated index.js challenge g

--- IWA_Challenges-main/Challenge 4/Challenge g/script.js
+++ IWA_Challenges-main/Challenge 4/Challenge g/script.js
@@ -1,34 +1,46 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+let FREE_WARNING = 'Free shipping only applies to single customer orders'
+let BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+let NONE_SELECTED = '0'
+let shipping = null
+let currency = null
+let customers = '1'
+const location = 'RSA'
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+if (location === 'RSA') { 
+	shipping === 400 
+	currency === 'R' }
 
-if location = NAM
-shipping = 600 
-else shipping = 800
+else if (location === NAM){
+     shipping = 600 
+	 currency = '$'
+	}
+	else if (location === 'NK'){
+		console.log(BANNED_WARNIN)
+	}
+else {
+	shipping = 800
+	currency = "$"
+}
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * 'NONE_SELECTED'
+let batteries = 35 * 2
+let pens = 5 * 'NONE_SELECTED' 
 
-shipping = null
-currency = $
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
+
+if ((shoes + batteries + pens + shirts) >= 1000) {{
+	if ((location = 'NAM' || location === "RSA") && (customers === 1)) {
+		    shipping = 0 
+			console.log(FREE_WARNING)
 		}
 	}
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+if (shipping === 0 && customers !== 1) { console.log(WARNING) }
 
 location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-customers = '1'
-const location = 'RSA'
-currency = null
+
+
