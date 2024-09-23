b7c808d0f8c8762a24d118ced784e3003e7ea0fc
Thando <www.thandwem045@gmail.com>
2024-09-23 11:58:51 +0200

    Debugged script.js challenge i

--- IWA_Challenges-main/Challenge 5/Challenge i/script.js
+++ IWA_Challenges-main/Challenge 5/Challenge i/script.js
@@ -1,17 +1,17 @@
 const rent = 400;
-const tax = '8%';
+const tax = 8;
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = '00';
+const minuteOfDay = '00';
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+if (((hourOfDay !== null) && (minuteOfDay !== null)) && ((hourOfDay == '00') && (minuteOfDay == '00'))) {
+	const taxAsDecimal = tax / 100     // gives us percentage of tax
+    const startingAfterTax = salary - ( salary * taxAsDecimal); 
+	const balance = startingAfterTax - transport - food - rent;
+    console.log('R' + balance.toFixed(2))
+
 }
-	
-console.log(balance.toFixed(3))
