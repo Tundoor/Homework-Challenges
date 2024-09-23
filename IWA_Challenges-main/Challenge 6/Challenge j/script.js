10f5e375fe7f772a23c859cf340a400e29dd65f6
Thando <www.thandwem045@gmail.com>
2024-09-23 12:15:08 +0200

    Debugged script.js challenge j

--- IWA_Challenges-main/Challenge 6/Challenge j/script.js
+++ IWA_Challenges-main/Challenge 6/Challenge j/script.js
@@ -1,2 +1,6 @@
-const value = "3"
+const value = 3
 console.log(value + 4 + value)
+
+/* 343 was logged initially because the value variable was pointing to a string. When we logged our value it concatenated rather than add 
+ because strings aren't numbers and won't work for mathematical expressions. The addition operator joins strings together but doesn't add them 
+even if there is a number between the quotes which is another reason why it did not work. */
